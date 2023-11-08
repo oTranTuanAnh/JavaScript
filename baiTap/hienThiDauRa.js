070afbdc8e6d92e890023d101796d5103712651f
tuananhpaddy <tuananhtran.xd@gmail.com>
2023-11-08 11:52:25 +0700

    first add

---
+++ baiTap/hienThiDauRa.js
@@ -0,0 +1,17 @@
+// function showMessage() {
+//     alert('Xin chao');
+// }
+
+// function showMessage() {
+//     document.getElementById('mesageDiv').innerHTML='Xin chao';
+// }
+
+
+//
+// function showMessage() {
+//     document.write('xin chao');
+// }
+
+function showMessage() {
+    console.log('xin chao chao chao')
+}
---
+++ js.js
@@ -0,0 +1,8 @@
+
+// let demo = document.getElementById("bt");
+// demo.addEventListener("click",clickfunction);
+// function clickfunction() {
+//     alert("xin chao")
+// }
+ten =  prompt("nhap du lieu");
+alert(ten)
